8d155398fb11e72491e0121ca4e5edd8fe7f3bee
Sergio <dsbox@list.ru>
2022-02-06 20:02:29 +0300

    nav created, adding auth forms

--- client/src/components/Registration/Registration.jsx
+++ client/src/components/Registration/Registration.jsx
@@ -7,7 +7,7 @@ function Registration(props) {
       <input name="email" type="email" placeholder="Введите адрес электронной почты" />
       <input name="password" type="password" placeholder="Введите пароль" />
       <input name="password" type="password" placeholder="Повторите пароль" />
-      <input type="button" value="Отправить" />
+      <input type="button" value="Отправить" /> 
     </form>
   );
 }
